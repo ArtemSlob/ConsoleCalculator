2b8626b553020ec18653c35519a3db64bd794a7a
DESKTOP <artem.slobodianiuk@gmail.com>
2021-10-21 15:38:08 +0300

    Реализация ввода

--- ConsoleCalculator/ConsoleCalculator/Calc.cs
+++ ConsoleCalculator/ConsoleCalculator/Calc.cs
@@ -6,7 +6,49 @@ using System.Threading.Tasks;
 
 namespace ConsoleCalculator
 {
-    class Calc
+    public static class Calc
     {
+        static public double Sum(double x, double y)
+        {
+            return x + y;
+        }
+
+        static public double Minus(double x, double y)
+        {
+            return x - y;
+        }
+
+        static public double Multiply(double x, double y)
+        {
+            return x * y;
+        }
+
+        static public double Divide(double x, double y)
+        {
+            while (y == 0)
+            {
+                Console.WriteLine("Can not divide by zero");
+                return 0;
+            }
+            return x / y;
+        }
+
+        static public double Sqrt(double num)
+        {
+            return Math.Sqrt(num);
+        }
+
+        static public double NumInput()
+        {
+            string numberString = Console.ReadLine();
+            bool isDoubleInput = double.TryParse(numberString, out double numberDouble);
+            while (!isDoubleInput)
+            {
+                Console.Write("Wrong input! Try again: ");
+                numberString = Console.ReadLine();
+                isDoubleInput = double.TryParse(numberString, out numberDouble);
+            }
+            return numberDouble;
+        }
     }
 }
